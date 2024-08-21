82c949a164a4dcf1de288dd1ac26372fd43ebf02
kaustubhk134 <kulurkarkaustubh134@gmail.com>
2024-08-21 20:20:20 +0530

    area of circle

---
+++ P3area_of_circle.java
@@ -0,0 +1,14 @@
+//this code is to find the area of the circle.
+import java.util.*;
+public class P3area_of_circle {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the Radius of the Circle : ");
+        float rad = sc.nextFloat();
+
+        System.out.println("Area of Circle is : "+ (3.14*rad*rad));
+
+    }
+    
+}
