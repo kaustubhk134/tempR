9108b055da59adc432fa06f4526dbfffb5db9766
kaustubhk134 <kulurkarkaustubh134@gmail.com>
2024-08-23 20:20:20 +0530

    else if

---
+++ P4if_else.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class P4if_else {
+    public static void main (String args[]){
+        int age = 17;
+        if (age >= 18){
+            System.out.println("The person is ADULT");
+        }else{
+            System.out.println("The person is MINOR");
+        }
+    }
+    
+}
